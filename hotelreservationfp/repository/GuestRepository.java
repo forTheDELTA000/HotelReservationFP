d633caac93644ef67ca1e0c9b2830de8ee81fc9f
Just-SleepPls <patriciocarl235@gmail.com>
2025-11-11 13:15:47 +0800

    Delete hotelreservationfp directory
    mali ang psuh

--- hotelreservationfp/repository/GuestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hotelreservationfp.repository;
-
-import com.example.hotelreservationfp.entity.Guest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuestRepository extends JpaRepository<Guest, Integer> {
-    Guest findByEmail(String email);
-}
--- hotelreservationfp/repository/RoomTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hotelreservationfp.repository;
-
-import com.example.hotelreservationfp.entity.RoomType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoomTypeRepository extends JpaRepository<RoomType, Integer> {
-}
